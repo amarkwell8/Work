a640260e9b4849f72803006ec07ca204f84da0e7
Austin Markwell <amarkwell8@gmail.com>
2016-04-13 16:33:39 -0700

    Project to learn WPF
    Initial upload functionality: Upload 2 files using a file browser, and the submit button will read the files back.

---
+++ LearnWPF/LearnWPF/Input.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LearnWPF
+{
+    /// <summary>
+    /// Interaction logic for Input.xaml
+    /// </summary>
+    public partial class Input : Page
+    {
+        public Input()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                TextBox1.Text = filename;
+            }
+        }
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            // Set filter for file extension and default file extension 
+            dlg.DefaultExt = ".txt";
+            dlg.Filter = "Text documents (.txt)|*.txt";
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                TextBox2.Text = filename;
+            }
+        }
+        private void Submit(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string input1;
+                StreamReader file1 = new StreamReader(TextBox1.Text);
+
+                while((input1 = file1.ReadLine()) != null)
+                {
+                    MessageBox.Show(input1);
+                }
+
+                file1.Close();
+            }
+            catch
+            {
+                
+            }
+        }
+}
+}
